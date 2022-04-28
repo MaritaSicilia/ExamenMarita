2bdb578f41d0af751f9aa87a664c41926f8f7721
MaritaSicilia <90844940+MaritaSicilia@users.noreply.github.com>
2022-04-28 11:59:07 +0200

    commit clase suma

---
+++ prueba1/src/prueba1/Suma.java
@@ -0,0 +1,26 @@
+package prueba1;
+
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		int num1, num2, result;
+		
+
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Introduzca el primer numero");
+		num1 = sc.nextInt();
+		
+		System.out.println("Introduzca el segundo número");
+		num2 = sc.nextInt();
+		
+		
+		result = num1 + num2;
+		System.out.println("El resultado de la suma es:" + result);
+
+		
+	}
+
+}
